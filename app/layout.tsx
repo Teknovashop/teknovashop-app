a077bf99c03e20c3b50583b34ab7d9168f26981d
Teknovashop <teknovashop25@gmail.com>
2025-09-12 20:30:42 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+// app/layout.tsx
+import type { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
