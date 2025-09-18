30d99f1c10775b34fd48fe2c7181c0009a36b08f
Teknovashop <teknovashop25@gmail.com>
2025-09-18 19:20:51 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
 export default {
-  reactStrictMode: true,
-  // No pre-renderices rutas /api del backend externo
-  // y evita que Next intente "exportarlas" en build.
-  output: 'standalone',
+  reactStrictMode: true
 };
