f80cabc351713910c4f84d4474822d01297f07ec
Teknovashop <teknovashop25@gmail.com>
2025-10-10 16:09:22 +0200

    Delete lib/flags.ts

--- lib/flags.ts
+++
@@ -1,6 +0,0 @@
-// lib/flags.ts
-export const FLAGS = {
-  v2: process.env.NEXT_PUBLIC_FORGE_V2 === "1",
-  laser: process.env.NEXT_PUBLIC_FORGE_LASER === "1",
-  text: process.env.NEXT_PUBLIC_FORGE_TEXT === "1",
-};
