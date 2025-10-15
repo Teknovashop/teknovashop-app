0b1e9f5642d1a6436e3a7fff3266b9306cd9724d
Teknovashop <teknovashop25@gmail.com>
2025-10-15 13:49:57 +0200

    Delete components/STLViewer.tsx

--- components/STLViewer.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-export default function STLViewer() {
-  return (
-    <div className="aspect-video w-full rounded-xl border border-dashed border-black/20 md:border-white/20 grid place-items-center">
-      <span className="text-sm text-black/60 md:text-white/70">STLViewer (placeholder)</span>
-    </div>
-  );
-}
